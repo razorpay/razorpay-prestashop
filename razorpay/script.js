5a7841b48a307353fc1095008ec8b7a57941909f
Ram Prakash Singh <ram.techchef@razorpay.com>
2019-05-28 11:27:45 +0530

    #PAY-1289: Sending checkout variable prestashop_order_id with empty value in notes, as it required for API integartion

--- razorpay/script.js
+++ razorpay/script.js
@@ -61,6 +61,7 @@ document.addEventListener('DOMContentLoaded', function(event) {
       },
       order_id: defaults.rzp_order_id,
       notes: {
+        prestashop_order_id: '',
         prestashop_cart_id: defaults.cart_id,
       },
       handler: function(obj) {
