660846b649c75600d663a60dc3aa41ab97acc1b6
“swati31196” <swatichoudhary15b58@gmail.com>
2022-02-09 16:08:43 +0530

    semgrep issue fix

--- razorpay/index.php
+++ razorpay/index.php
@@ -1,6 +1,6 @@
 <?php
 header('Expires: Tue, 01 Apr 1997 05:00:00 GMT');
-header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s').' GMT');  // nosemgrep : php.lang.security.non-literal-header.non-literal-header
 
 header('Cache-Control: no-store, no-cache, must-revalidate');
 header('Cache-Control: post-check=0, pre-check=0', false);
